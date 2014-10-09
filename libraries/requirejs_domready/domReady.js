eff4d4144f4d97508f237ba691f5fa934c657004
Attila Beregszaszi <attila@frontseed.com>
2014-10-09 12:54:18 +0100

    Fixed dummy domReady to actually return the document object

--- libraries/requirejs_domready/domReady.js
+++ libraries/requirejs_domready/domReady.js
@@ -1,17 +1,27 @@
 /**
  * @file
- * This is a dummy replacement for the requirejs domReady plugin as the full
- * plugin is placed inline.
- *
- * This file is kept for r.js optimisation via grunt.
+ * This is a dummy replacement for the requirejs domReady plugin to retain
+ * backwards compatibility with older applications.
  *
  * @see  dennis_js.module
  * @see  dennis_js_get_js()
  */
-define(function () {
+define(function() {
   'use strict';
 
-  function domReady() {
+  var isBrowser = typeof window !== 'undefined' && window.document,
+    doc = isBrowser ? document : null;
+
+  /**
+   * Registers a callback for DOM ready. Since this dummy plugin can only run if
+   * the DOM is actually already ready, we do not perform any specific stunts
+   * here, we just call the callback immediately.
+   *
+   * @param {Function} callback
+   */
+  function domReady(callback) {
+    callback(doc);
+
     return domReady;
   }
 
@@ -20,8 +30,13 @@ define(function () {
   /**
    * Loader Plugin API method
    */
-  domReady.load = function (name, req, onLoad) {
-    onLoad(null);
+  domReady.load = function (name, req, onLoad, config) {
+    if (config.isBuild) {
+      onLoad(null);
+    }
+    else {
+      domReady(onLoad);
+    }
   };
 
   return domReady;
